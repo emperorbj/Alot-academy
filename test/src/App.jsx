783aa7e2f82547be5135b2874271b3964a1ae895
emperorbj <cwaku96@gmail.com>
2024-04-29 18:31:51 -0700

    added form component

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Login from './components/Login'
 
 function App() {
 
   return (
-    <>
-
-    </>
+      <>
+        <Login/>
+      </>
   )
 }
 
---
+++ test/src/components/Login.jsx
@@ -0,0 +1,42 @@
+
+
+const Login = () => {
+    return (
+        // form container
+        <div className="flex justify-center items-center h-screen">
+            <form className="bg-slate-900 w-1/2 flex flex-col items-center h-96" action="">
+                <h1 className="text-white text-6xl font-light pt-16">Welcome Back</h1>
+                <p className="text-slate-300">Please sign into your account</p>
+                {/* email */}
+                <div>
+                    <label className="text-slate-300" htmlFor="email">your email here</label>
+                    <input className="text-slate-300" type="text" placeholder='email here' />
+                </div>
+                {/* password */}
+                <div>
+                    <label className="text-slate-300" htmlFor="password">password</label>
+                    <input className="text-slate-300" type="password" required placeholder='password here' />
+                </div>
+                <div>
+                    {/* check box remember me password */}
+                    <div>
+                        <input type="checkbox" name="remember"/>
+                        <p className="text-slate-300">Remember me</p>
+                    </div>
+                    {/* forgot password */}
+                    <div>
+                        <p className="text-slate-300">Forgot password?</p>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <button type="submit">SIGN IN</button>
+                    <button>Or continue with</button>
+                    <button><span>icon</span>Google</button>
+                    <button><span>icon</span>Facebook</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login
