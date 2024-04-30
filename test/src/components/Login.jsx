4c947e460454a7fb9387b3307cdebb0cee0ffabe
emperorbj <cwaku96@gmail.com>
2024-04-30 02:40:45 -0700

    designed the background of form

--- test/src/components/Login.jsx
+++ test/src/components/Login.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Google from '../assets/google.png';
+import Facebook from '../assets/facebook.png';
+import Help from '../assets/Property 34.png';
+import Vector1 from '../assets/vectors/Vector (1).png';
+import Vector2 from '../assets/vectors/Vector (2).png';
+import Vector3 from '../assets/vectors/Vector (3).png';
+import Vector4 from '../assets/vectors/Vector (4).png';
+import Vector5 from '../assets/vectors/Vector (5).png';
+import Vector6 from '../assets/vectors/Vector (6).png'
+import Vector7 from '../assets/vectors/Vector (7).png';
+
+
 
 
 const Login = () => {
     return (
         <div>
             <Navbar/>
-            <div className="flex justify-center items-center h-screen">
-                <form className="bg-slate-900 w-[531px] flex flex-col items-center h-[520px] my-4" action="">
+            <div className="relative z-0 flex justify-center items-center bg-slate-900 h-screen">
+                <img className="absolute z-10 top-[5px] left-[-30px]" src={Vector1} alt="" width={252} height={247} />
+                <img className="absolute z-30 rotate-[270deg] top-[-30px] left-[350px]" src={Vector2} alt="" width={131} height={203}/>
+                <img className="absolute z-10 right-[0px] top-[-60px]" src={Vector3} alt="" width={100} height={30} />
+                <img className="absolute z-10 top-[70px] right-[200px]" src={Vector4} alt="" width={529} height={538} />
+                <img className="absolute z-10 left-[0px] bottom-[0px]" src={Vector5} alt="" width={535} height={510} />
+                <img className="absolute z-10 bottom-[0px] right-[0px]" src={Vector6} alt="" width={455} height={110} />
+                <img className="absolute z-10 top-[330px] right-[0px]" src={Vector7} alt="" width={131} height={203}/>
+                <div className="absolute top-[100px] right-[150px] flex items-center rounded-full justify-center z-10 border-2 border-slate-400 w-[70px] h-[70px]">
+                        <img className="bg-slate-400 rounded-full w-[50px] h-[50px]" src={Help} alt=""/>
+                </div>
+                <form className="bg-slate-900 z-20 w-[471px] flex flex-col items-center h-[550px] mt-[80px]" action="">
                     <h1 className="text-white text-5xl font-light pt-8">Welcome Back</h1>
                     <p className="text-slate-300 mt-4">Please sign into your account</p>
                     {/* email */}
@@ -23,7 +45,7 @@ const Login = () => {
                     <div className="flex justify-between w-[100%] mt-4">
                         {/* check box remember me password */}
                         <div className="flex ml-16">
-                            <input className="w-4 h-4 mt-1" type="checkbox" name="remember"/>
+                            <input className="w-4 h-4 mt-1 border-2 outline-none" type="checkbox" name="remember"/>
                             <p className="text-slate-300 ml-2">Remember me</p>
                         </div>
                         {/* forgot password */}
@@ -34,8 +56,14 @@ const Login = () => {
                     <div className="flex flex-col">
                         <button className="bg-[#FBE25C] mt-4 w-[421px] h-[40px] text-2xl font-semibold rounded-lg" type="submit">SIGN IN</button>
                         <button className="w-[421px] mt-1 h-[30px] text-xl font-normal text-slate-200">Or continue with</button>
-                        <button className="w-[421px] bg-slate-800 mt-1 h-[30px] text-lg text-slate-200 rounded-lg shadow-lg"><span>icon</span>Google</button>
-                        <button className="w-[421px] bg-slate-800 mt-2 mb-4 h-[30px] text-lg text-slate-200 rounded-lg shadow-lg"><span>icon</span>Facebook</button>
+                        <div id="google" className=" flex justify-center w-[421px] py-2  bg-slate-800 mt-1 h-[35px] text-lg text-slate-200 rounded-lg shadow-lg">
+                            <img src={Google} alt="" height={10} width={15}/>
+                            <p className="ml-[10px] mt-[-4px]">Google</p>
+                        </div>
+                        <div id="facebook" className="flex justify-center py-2 w-[421px] bg-slate-800 mt-2 mb-6 h-[35px] text-lg text-slate-200 rounded-lg shadow-lg">
+                            <img src={Facebook} alt="" height={10} width={15}/>
+                            <p className="ml-[10px] mt-[-4px]">Facebook</p>
+                        </div>
                     </div>
                 </form>
             </div>
--- test/src/components/Navbar.jsx
+++ test/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className='bg-blue-400'>
+        <div className='bg-blue-400 fixed z-30 h-10 w-full'>
             <h1>navigation</h1>
         </div>
     )
