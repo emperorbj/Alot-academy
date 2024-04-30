2c9a7824dea16daf5080b454008fa7000d668ca2
emperorbj <cwaku96@gmail.com>
2024-04-29 20:59:14 -0700

    updated form

--- test/src/components/Login.jsx
+++ test/src/components/Login.jsx
@@ -5,12 +5,12 @@ const Login = () => {
         // form container
         <div className="flex justify-center items-center h-screen">
             <form className="bg-slate-900 w-1/2 flex flex-col items-center h-96" action="">
-                <h1 className="text-white text-6xl font-light pt-16">Welcome Back</h1>
+                <h1 className="text-white text-5xl font-light pt-16">Welcome Back</h1>
                 <p className="text-slate-300">Please sign into your account</p>
                 {/* email */}
-                <div>
-                    <label className="text-slate-300" htmlFor="email">your email here</label>
-                    <input className="text-slate-300" type="text" placeholder='email here' />
+                <div className="flex flex-col">
+                    <label className="text-slate-300" htmlFor="email">Email address</label>
+                    <input className="text-slate-300 bg-slate-800 w-72" type="text" placeholder='example@gmail.com' />
                 </div>
                 {/* password */}
                 <div>
